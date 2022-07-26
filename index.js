e3acee5be32dd5fe40fd9bd8149bc869cf690853
Aman2104 <uietcseamankumar2125@kuk.ac.in>
2022-07-26 11:25:17 +0530

    New

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let btn1= document.getElementById("menu");
-let list= document.getElementsByClassName("navbar-links");
+let btn1 = document.getElementById("menu");
+let hello = document.getElementsByClassName("navbar-links");
 let nav = document.getElementById("nav1");
-btn1.addEventListener('click', ()=>{
-    for(var i =0; i<list.length; i++)
-    list[i].classList.toggle("act");
+btn1.addEventListener('click', () => {
+    for (var i = 0; i < hello.length; i++) {
+        hello[i].classList.toggle("act");
+    }
     //  font-family: 'Edu TAS Beginner', cursive;
 })
